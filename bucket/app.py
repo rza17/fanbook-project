b2237580f90cd3827a946f51cc1799d731a71381
Reza Smith <rezabinsmith17@gmail.com>
2023-02-06 22:38:00 +0800

    flask setup for deployment

--- bucket/app.py
+++ bucket/app.py
@@ -1,8 +1,20 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://reza:smith@ac-pjtljuw-shard-00-00.rr6j0to.mongodb.net:27017,ac-pjtljuw-shard-00-01.rr6j0to.mongodb.net:27017,ac-pjtljuw-shard-00-02.rr6j0to.mongodb.net:27017/?ssl=true&replicaSet=atlas-sk6hep-shard-0&authSource=admin&retryWrites=true&w=majority')
-db = client.dbsparta
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
+
 
 app = Flask(__name__)
 
